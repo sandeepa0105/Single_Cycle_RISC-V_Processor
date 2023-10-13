e882c35a259268b289f93057dc7133aebeb97c02
Sandeepa Dilshan <dilshansandeepa722@gmail.com>
2023-10-13 11:50:26 +0530

    Create right_shifter.sv

---
+++ right_shifter.sv
@@ -0,0 +1,7 @@
+module right_shifter(
+    input wire [31:0] shift_in,   // 2-bit input data
+    output reg [31:0] shift_out  // 2-bit left-shifted output data
+);
+assign shift_out = shift_in >> 2; // Left shift data_in by 1 bit
+
+endmodule
